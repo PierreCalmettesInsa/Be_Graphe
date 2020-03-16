71ee478a0763d87815d57cf153dc95c9115f02a8
pierre <pierrecalmettes228@gmail.com>
2020-03-16 10:55:13 +0100

    méthode getLength() terminée

--- be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
@@ -210,11 +210,13 @@ public class Path {
      * 
      * @return Total length of the path (in meters).
      * 
-     * @deprecated Need to be implemented.
      */
     public float getLength() {
-        // TODO:
-        return 0;
+    	float tailleTot = 0 ;
+        for (Arc arc : this.arcs) {
+        	tailleTot += arc.getLength();
+        }
+        return tailleTot;
     }
 
     /**
