31999622c62c5805f529f5b74e36066705af1b5f
pierre <pierrecalmettes228@gmail.com>
2020-03-16 11:04:26 +0100

    méthode getTRavelTime() terminée

--- be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
@@ -227,11 +227,13 @@ public class Path {
      * @return Time (in seconds) required to travel this path at the given speed (in
      *         kilometers-per-hour).
      * 
-     * @deprecated Need to be implemented.
      */
     public double getTravelTime(double speed) {
-        // TODO:
-        return 0;
+        double timeTot = 0 ;
+        for (Arc arc : this.arcs) {
+        	timeTot += arc.getTravelTime(speed);
+        }
+        return timeTot;
     }
 
     /**
